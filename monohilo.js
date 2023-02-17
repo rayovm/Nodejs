cb19f8df67d62485335dab11601608a049d4f6b4
Raymundo Vazquez <raymundo.vazquez@gmail.com>
2023-02-17 00:15:47 -0600

    promises

---
+++ monohilo.js
@@ -0,0 +1,16 @@
+const { Z_ASCII } = require("zlib");
+
+console.log("Hola Mundo");
+
+let i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+
+    if (i === 5) {
+        console.log("forzamos error");
+        var a = 3 + z;
+    }
+}, 1000);
+
+console.log("Segunda instrucción");
